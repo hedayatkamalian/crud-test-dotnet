7860398e5bd989c3a70a44a83ba6f944899c703d
Hedayat Kamalian <Kamalian@must.it>
2024-02-28 23:35:54 +0330

    fix long roundup bug in json

--- Mc2.CrudTest.Application/Dtos/CustomerDto.cs
+++ Mc2.CrudTest.Application/Dtos/CustomerDto.cs
@@ -2,7 +2,7 @@
 
 public class CustomerDto
 {
-    public long Id { get; set; }
+    public string Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string PhoneNumber { get; set; }
